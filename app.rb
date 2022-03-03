14eb8dcbb06a09946b4ce37861e5a79b00dc7d4c
Amkam <amaduskamara@gmail.com>
2022-03-03 16:38:52 +0000

    Add all_books, new-student, new-teacher, new-person and new_book methods

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
   def run_app
     puts
     puts "** Welcome to OOP School Library App **"
@@ -7,6 +13,7 @@ class App
   end
   
   def start_app
+    puts "Please choose option by entering a number: "
     puts "1. List all books."
     puts "2. List all people."
     puts "3. Create a person"
@@ -39,7 +46,15 @@ class App
   end
 
   def all_books
-
+    if @books.length.zero?
+      puts
+      puts "There's no book, Please add a book first"
+      puts
+    else
+      @books.each_with_index do |book, index|
+        puts "#{index + 1} - Book title: #{book.title}, Written by: #{book.author}"
+      end
+    end
   end
 
   def all_people
@@ -47,11 +62,58 @@ class App
   end
 
   def new_person
+    puts
+    puts "Do you want to create a: \n1. Student \n2. Teacher"
+    option = gets.chomp.to_i
+    case option
+    when 1
+      @persons.push(new_student)
+      puts 'Student created successfully!'
+    when 2
+      @persons.push(new_teacher)
+      puts 'Teacher created successfully!'
+    else
+      puts 'Please select a correct option'
+      create_person
+    end
+  end
 
+  def new_student
+    print 'Student name: '
+    name = gets.chomp
+    print 'Student age: '
+    age = gets.chomp
+    print "Has parent permission? [Y/N - y/n]: "
+    permission = gets.chomp
+    puts
+    case permission
+    when "Y" || "y"
+      Student.new(age, name)
+    when "N" || "n"
+      Student.new(age, name, parent_permission: false)
+    else
+      puts 'Please select a correct option'
+      create_student
+    end
+  end
+
+  def new_teacher
+    print "Teacher age: "
+    age = gets.chomp
+    print "Teacher name: "
+    name = gets.chomp
+    print "Specialization: "
+    specialization = gets.chomp
+    Teacher.new(age, specialization, name)
   end
 
   def new_book
-
+    print "Book title: "
+    title = gets.chomp
+    print "Book author: "
+    author = gets.chomp
+    @books.push(Book.new(title, author))
+    puts "✔️ Book created successfully"
   end
 
   def new_rental
@@ -61,5 +123,4 @@ class App
   def all_rentals
 
   end
-
 end
