6027e1c3106ba295a339b441185da91ae5020c61
Amkam <amaduskamara@gmail.com>
2022-03-03 15:31:50 +0000

    Add all methods and get input from a user

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 class App
   def run_app
+    puts
     puts "** Welcome to OOP School Library App **"
+    puts
     start_app
   end
   
@@ -14,7 +16,50 @@ class App
     puts "7. Exit"
     puts
     input = gets.chomp.to_i
+    handle_input(input)
+    start_app()
   end
 
-  
+  def handle_input(input)
+    case input
+      when 1 then all_books
+      when 2 then all_people
+      when 3 then new_person
+      when 4 then new_book
+      when 5 then new_rental
+      when 6 then all_rentals
+      when 7 then
+        puts "Thank you for using OOP School Library app"
+        exit
+      else
+        puts
+        puts "Sorry, input not specified. Try again next time."
+        puts
+    end
+  end
+
+  def all_books
+
+  end
+
+  def all_people
+
+  end
+
+  def new_person
+
+  end
+
+  def new_book
+
+  end
+
+  def new_rental
+
+  end
+
+  def all_rentals
+
+  end
+
 end
