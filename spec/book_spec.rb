b877023b3fab5cf1e4d46a7d5660416c64d3198e
Amkam <amaduskamara@gmail.com>
2022-03-10 10:12:50 +0000

    Edit expect sentences in book_spec.rb"

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,11 +4,11 @@ describe Book do
   context "Testing for book class" do
     book = Book.new("The Way of Kings", "Brandon Sanderson")
 
-    it "Should return the author of the book" do
+    it "should return the author of the book" do
       expect(book.author).to eq "Brandon Sanderson"
     end
 
-    it "Should return the title of the book" do
+    it "should return the title of the book" do
       expect(book.title).to eq "The Way of Kings"
     end
   end
