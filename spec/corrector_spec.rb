cc7e002f86def5c5509efe1342736bd3ae33d465
Amkam <amaduskamara@gmail.com>
2022-03-10 13:29:56 +0000

    Write test for corrector class

---
+++ spec/corrector_spec.rb
@@ -0,0 +1,10 @@
+require './corrector'
+
+describe Corrector do
+  context "Testing for Corrector class" do
+    corrector = Corrector.new()
+    it "should trim string to 10 length" do
+      expect(corrector.correct_name("amkamwebavanger")).to eq "Amkamwebav"
+    end
+  end
+end
